94b06fbf834f02f4e05af5e6113429db0fb1b3f0
Mohammed Raja <happygreetingtoyou@gmail.com>
2025-12-25 17:50:18 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD5bztc2AmaP_p3aYLz6F-Zk79I62scOYQ",
   authDomain: "happy-greeting-billing.firebaseapp.com",
@@ -16,6 +18,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
