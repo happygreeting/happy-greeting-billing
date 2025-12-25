270b1a6c6e7c475fba9ee06ec22d51eb948ed16f
Mohammed Raja <happygreetingtoyou@gmail.com>
2025-12-25 17:59:08 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD5bztc2AmaP_p3aYLz6F-Zk79I62scOYQ",
+  apiKey: "AIzaSyD5bZtc2AmaP_p3aYzL6F-Zk79162csOYQ",
   authDomain: "happy-greeting-billing.firebaseapp.com",
   projectId: "happy-greeting-billing",
-  storageBucket: "happy-greeting-billing.firebasestorage.app",
+  storageBucket: "happy-greeting-billing.appspot.com",
   messagingSenderId: "853948169436",
-  appId: "1:853948169436:web:6f611f6d18ac3e09238819",
-  measurementId: "G-6S5VXNKK3E"
+  appId: "1:853948169436:web:6f611f6d18ac3e09238819"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
