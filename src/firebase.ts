01776f1fb09a42e6109032829cb631e2a7f031d1
Mohammed Raja <happygreetingtoyou@gmail.com>
2025-12-29 01:54:10 +0530

    remove hardcoded firebase secrets

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,19 +3,18 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD5bztc2AmaP_p3aYLz6F-Zk79I62scOYQ",
-  authDomain: "happy-greeting-billing.firebaseapp.com",
-  projectId: "happy-greeting-billing",
-  storageBucket: "happy-greeting-billing.firebasestorage.app",
-  messagingSenderId: "853948169436",
-  appId: "1:853948169436:web:6f611f6d18ac3e09238819",
-  measurementId: "G-6S5VXNKK3E"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const analytics = getAnalytics(app);
